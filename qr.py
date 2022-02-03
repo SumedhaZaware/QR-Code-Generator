a8c58a4bc5ba7772b081fc29dfea655c3651af29
Sumedha Nitin Zaware <91329268+SumedhaZaware@users.noreply.github.com>
2022-02-03 14:27:18 +0530

    Add files via upload

---
+++ qr.py
@@ -0,0 +1,41 @@
+'''
+Created by: Sumedha Zaware
+'''
+
+# Import required libraries
+import tkinter as tk
+import qrcode
+
+# Setup the screen
+root = tk.Tk()
+root.title('QR Code Generator')
+root.geometry('400x200')
+root.config(bg="#ECD6D1")
+
+def save_qr():
+    ''' Save the generated QR Code for the given input text or link
+    '''
+    data = link_entry.get()
+    img = qrcode.make(data)
+    img.save('output.png')
+
+    text = tk.Label(root,text='QR Code generated and saved!!',bg="#ECD6D1")
+    text.place(x=70,y=120)
+    text.config(font=('Helvatical bold',10))
+
+# Link label
+link_label = tk.Label(root, text="Enter link or text:" ,bg="#ECD6D1")
+link_label.place(x=20, y=20)
+link_label.config(font=('Helvatical bold',16))
+
+# Link or text entry
+link_entry = tk.Entry(root)
+link_entry.place(x=70, y=40)
+link_entry.config(font=('Helvatical bold',12))
+
+# Button to generate QR code
+# , fg="white",bg='black',padx=2
+generate_button = tk.Button(root, text ="Generate QR Code", command = save_qr)
+generate_button.place(x=120,y=80)
+
+root.mainloop()
