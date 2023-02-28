b98b14b4366a396f10d70dd21d0a19ef3e655510
Dev V Juneja <78873223+devfinwiz@users.noreply.github.com>
2023-02-28 12:32:08 +0530

    url validation method added

--- qr.py
+++ qr.py
@@ -12,16 +12,32 @@ root.title('QR Code Generator')
 root.geometry('400x200')
 root.config(bg="#ECD6D1")
 
+def url_checker(url):
+	try:
+		#Get Url
+		get = requests.get(url)
+		# if the request succeeds 
+		if get.status_code == 200:
+			return True
+		else:
+			return False
+
+	#Exception
+	except requests.exceptions.RequestException as e:
+        # print URL with Errs
+		raise SystemExit(f"{url}: is Not reachable \nErr: {e}")
+
 def save_qr():
     ''' Save the generated QR Code for the given input text or link
     '''
     data = link_entry.get()
-    img = qrcode.make(data)
-    img.save('output.png')
+    if(url_checker(data)):
+        img = qrcode.make(data)
+        img.save('output.png')
 
-    text = tk.Label(root,text='QR Code generated and saved!!',bg="#ECD6D1")
-    text.place(x=70,y=120)
-    text.config(font=('Helvatical',10))
+        text = tk.Label(root,text='QR Code generated and saved!!',bg="#ECD6D1")
+        text.place(x=70,y=120)
+        text.config(font=('Helvatical',10))
 
 # Link label
 link_label = tk.Label(root, text="Enter link or text:" ,bg="#ECD6D1")
